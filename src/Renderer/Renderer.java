fbf2d05b3b74aed5b096e4ae40251c72bae01bd1
Christopher Russell <russelchri@ecs.vuw.ac.nz>
2020-09-14 19:30:21 +1200

    Turned Renderer into a Canvas to draw on

--- src/Renderer/Renderer.java
+++ src/Renderer/Renderer.java
@@ -1,7 +1,24 @@
 package Renderer;
 
-public class Renderer {
+import java.awt.*;
+
+/**
+ * This is responsible for displaying the Maze on the screen
+ * including all it's tiles, animations and sound effects
+ * @author Chris
+ */
+public class Renderer extends Canvas {
     public Renderer(){
 
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        Graphics2D g2 = (Graphics2D) g; //Graphics 2D gives you more drawing options
+
+        //Set background
+        g2.setColor(Color.WHITE);
+        g2.fillRect(0, 0, getWidth(), getHeight());
     }
 }
