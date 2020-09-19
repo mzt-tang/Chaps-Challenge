094a5d2fb6ba2754d5c08f6ecb7f303088883236
Tang <MKT.Vorqull@gmail.com>
2020-09-19 17:02:32 +1200

    Edited the tile classes to fit in with the Game logic. Key now holds a String "colour" to compare to with LockedDoors.

---
+++ src/Maze/BoardObjects/Actors/PatternEnemy.java
@@ -0,0 +1,13 @@
+package Maze.BoardObjects.Actors;
+
+/**
+ * An actor that follows a specific pattern/route defined by it's constructor parameter.
+ */
+public class PatternEnemy {
+
+    private String route;
+
+    public PatternEnemy(String route) {
+        this.route = route;
+    }
+}
