c8f19bf686082e44d9876766f7ddc4a9b67b740f
Ben Darrah <hooflessmoofless@gmail.com>
2020-09-24 13:04:25 +1200

    Added JUnit, main method for MonkeyTest + supplementary classes

--- nz.ac.vuw.ecs.swen225.gp20/MonkeyTest/MonkeyTest.java
+++ nz.ac.vuw.ecs.swen225.gp20/MonkeyTest/MonkeyTest.java
@@ -1,4 +1,24 @@
 package MonkeyTest;
 
+import org.junit.*;
+import org.junit.runner.*;
+import org.junit.runner.notification.Failure;
+
+/**
+ * Runs full suite of tests, reports results
+ * 
+ * @author Ben
+ *
+ */
+
 public class MonkeyTest {
+	public static void main(String[] args) {
+	
+		Result result = JUnitCore.runClasses(ApplicationTests.class, MazeTests.class);
+		
+		for(Failure fail : result.getFailures()) { System.out.println(fail.toString());}
+		
+		System.out.println("Test Successful: " + result.wasSuccessful());
+	}
+	
 }
