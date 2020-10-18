b52312c7179f1027013e86930e7498ee66d0cfca
Ben Darrah <hooflessmoofless@gmail.com>
2020-10-18 17:00:54 +1300

    fixing stuff

--- nz.ac.vuw.ecs.swen225.gp20/MonkeyTest/TestSuite.java
+++ nz.ac.vuw.ecs.swen225.gp20/MonkeyTest/TestSuite.java
@@ -28,7 +28,7 @@ public class TestSuite {
 		// t = new ChapsChallenge();
 		// tester = t.getGame();
 		
-		tester =  new Game(board, new Player(new Position(4, 4)), 1); //PLACEHOLDER, need to implement level loader
+		tester =  new Game(board, new Player(new Position(4, 4)), null); //PLACEHOLDER, need to implement level loader
 		
 	}
 	
