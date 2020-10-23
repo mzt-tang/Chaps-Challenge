d6c609f419020f6f9829b16a90e539da4ba69890
Ben <hooflessmoofless@gmail.com>
2020-10-23 19:43:00 +1300

    reformatting

--- nz.ac.vuw.ecs.swen225.gp20/MonkeyTest/MonkeyTest.java
+++ nz.ac.vuw.ecs.swen225.gp20/MonkeyTest/MonkeyTest.java
@@ -1,10 +1,8 @@
 package MonkeyTest;
 
-import javax.swing.JOptionPane;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.RepeatedTest;
-
 import Maze.Board;
 import Persistence.Level;
 import Persistence.Persistence;
@@ -25,37 +23,47 @@ public class MonkeyTest {
 	static TestSuite test;
 	static Level level;
 
-@BeforeEach
-  void initialize() {
-	level = Persistence.getLevel(1);
-	board = new Board(level.getTileArray());
-	i = 500000;
-	 test = new TestSuite(board);
+	@BeforeEach
+	void initialize() {
+		level = Persistence.getLevel(1);
+		board = new Board(level.getTileArray());
+		i = 1000000;
+		test = new TestSuite(board);
 	}
 
-	
 
-	
+
+
+	/**
+	 * Plays the game for 30 seconds, regardless of how many commands are sent
+	 */
 	@Test
-	
-	public void runTimeTest() { //Plays the game for 30 seconds, regardless of how many commands are sent
+
+	public void runTimeTest() { 
 		long t = System.currentTimeMillis();
 		long end = t + 30000;
 		int count = 0;
 		while(System.currentTimeMillis() < end) {test.randomMovement(); count++;}
 		System.out.println("DEBUG: Time Test performed " + count + " commands");
-		}
-	
+	}
+
+	/**
+	 * 
+	 */
 	@Test
 	public void runNumberTest() {while( k < i) {test.randomMovement(); k++;}} //Runs tests for k number of commands, regardless of time taken
-	
+
+	/**
+	 * Similar to time test, designed to run a new game every 10 seconds to 
+	 * trial many different possible scenarios
+	 */
 	@RepeatedTest(15)
-	public void rapidTest() { // Similar to time test, designed to run a new game every 10 seconds to trial many different possible scenarios
+	public void rapidTest() {
 		long t = System.currentTimeMillis();
 		long end = t + 10000;
 		int count = 0;
 		while(System.currentTimeMillis() < end) {test.randomMovement(); count++;}
 	}
-	
+
 }
 
